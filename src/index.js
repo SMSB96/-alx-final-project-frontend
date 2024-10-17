84fb9e12943d7066e599cd3afdaeb367e0eaaf9c
SMSB96 <saramiad96@gmail.com>
2024-10-17 19:46:08 +0000

    tasks

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+import { PrimeReactProvider } from "primereact/api";
+
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+import "primereact/resources/primereact.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
